dd55242c3d77fa42670b9c47249388336f3382f2
Phillip Hassoun <philliphassoun@Phillips-MacBook-Air.local>
2025-10-14 22:12:50 -0400

    Removing prettier and adding heading component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,7 @@
-import { Stack } from "expo-router";
+import { Heading } from '@/components/ui/heading';
+import React from 'react';
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import '@/global.css';
-
 export default function RootLayout() {
-  return <Stack />;
+  return <Heading size={"sm"}>{"text"}</Heading>;
 }
